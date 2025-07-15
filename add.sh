4d3f1a6fe003253a689ea3009a71839fcdaf8221
Ubuntu <ubuntu@ip-172-31-7-54.ap-south-1.compute.internal>
2025-07-15 15:44:32 +0000

    saving the add.sh and sub.sh files

---
+++ add.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Prompt the user to enter two numbers
+echo "Enter first number:"
+read num1
+
+echo "Enter second number:"
+read num2
+
+# Perform addition
+sum=$((num1 + num2))
+
+# Display the result
+echo "The sum of $num1 and $num2 is: $sum"
+echo "Hiii"
+
---
+++ sub.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Prompt the user to enter two numbers
+echo "Enter the first number:"
+read num1
+
+echo "Enter the second number:"
+read num2
+
+# Perform subtraction
+diff=$((num1 - num2))
+
+# Display the result
+echo "The result of $num1 - $num2 is: $diff"
+echo "Hi this is from subtraction file"
+echo "new line"
+echo "new line no2"
